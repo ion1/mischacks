8c7f99c6bd297b9c0829de74efd8aac85866d7a1
Johan Kiviniemi <devel@johan.kiviniemi.name>
2009-06-28 17:22:56 +0300

    Release 0.0.4

--- lib/mischacks.rb
+++ lib/mischacks.rb
@@ -14,7 +14,7 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 module MiscHacks
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 
   class ChildError < RuntimeError
     attr_reader :status
